63984fade1f491d92ed2469f29ae6d299c2506b5
mu3 <74529749+doqltl179@users.noreply.github.com>
2025-07-07 22:49:47 +0900

    ResourceLoader 수정

--- Assets/Mu3LibraryAssets/Scripts/Utility/ResourceLoader.cs
+++ Assets/Mu3LibraryAssets/Scripts/Utility/ResourceLoader.cs
@@ -1,34 +1,67 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Mu3Library {
+using Object = UnityEngine.Object;
+
+namespace Mu3Library
+{
     /// <summary>
-    /// Resource를 캐싱하여 관리하기 위해 만듬
+    /// <br/> Resource를 캐싱하여 관리하기 위해 만듬
+    /// <br/> Resource를 반환하는 것으로 instantiate가 된 게임 오브젝트를 반환하는 것이 아님.
     /// </summary>
-    public static class ResourceLoader {
-        private static Dictionary<string, UnityEngine.Object> resources = new Dictionary<string, UnityEngine.Object>();
+    public static class ResourceLoader
+    {
+        private static Dictionary<string, Dictionary<Type, Object>> _resources = new Dictionary<string, Dictionary<Type, Object>>();
 
 
 
         /// <summary>
-        /// Resource를 반환하는 것으로 instantiate가 된 게임 오브젝트를 반환하는 것이 아님.
+        /// path: 파일 경로
         /// </summary>
-        public static T GetResource<T>(string path) where T : UnityEngine.Object {
-            UnityEngine.Object resource = null;
-            if(resources.TryGetValue(path, out resource)) {
-                return (T)resource;
-            }
-            else {
+        public static T GetResource<T>(string path) where T : Object
+        {
+            T resource = GetLoadedObject<T>(path);
+            if (resource == null)
+            {
                 resource = Resources.Load<T>(path);
-                if(resource == null) {
+                if (resource == null)
+                {
                     Debug.LogError($"Object not found in resources folder. path: {path}");
-
                     return null;
                 }
 
-                resources.Add(path, resource);
-                return (T)resource;
+                Dictionary<Type, Object> pathResources = null;
+                if (!_resources.TryGetValue(path, out pathResources))
+                {
+                    pathResources = new Dictionary<Type, Object>();
+                    _resources.Add(path, pathResources);
+                }
+
+                Type type = typeof(T);
+                pathResources.Add(type, resource);
             }
+
+            return resource;
+        }
+
+        /// <summary>
+        /// path: 파일 경로
+        /// </summary>
+        private static T GetLoadedObject<T>(string path) where T : Object
+        {
+            if (!_resources.ContainsKey(path))
+            {
+                return null;
+            }
+
+            Type type = typeof(T);
+            if (!_resources[path].ContainsKey(type))
+            {
+                return null;
+            }
+
+            return (T)_resources[path][type];
         }
     }
 }
