6b1a3e4046c87c60cc1984e3adde7b6a942d84e2
mu3 <74529749+doqltl179@users.noreply.github.com>
2025-10-03 21:22:11 +0900

    SceneLoader 샘플 추가

---
+++ Assets/Mu3LibraryAssets/Samples~/Sample_Game/Scripts/02_Main/MainSceneController.cs
@@ -0,0 +1,147 @@
+using System.Collections.Generic;
+using System.Linq;
+using Mu3Library.Editor.FileUtil;
+using Mu3Library.Scene;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mu3Library.Sample.Game
+{
+    public class MainSceneController : MonoBehaviour
+    {
+
+
+
+        private void Start()
+        {
+            SceneLoader.Instance.AddSceneLoadStartListener(StaticDatas.SCENE_NAME_GAME, TestStart);
+            SceneLoader.Instance.AddSceneLoadEndListener(StaticDatas.SCENE_NAME_GAME, TestEnd);
+            SceneLoader.Instance.AddSceneLoadProgressListener(StaticDatas.SCENE_NAME_GAME, TestProgress);
+        }
+
+        private void OnDestroy()
+        {
+            SceneLoader.Instance.RemoveSceneLoadStartListener(StaticDatas.SCENE_NAME_GAME, TestStart);
+            SceneLoader.Instance.RemoveSceneLoadEndListener(StaticDatas.SCENE_NAME_GAME, TestEnd);
+            SceneLoader.Instance.RemoveSceneLoadProgressListener(StaticDatas.SCENE_NAME_GAME, TestProgress);
+        }
+
+        private void TestStart()
+        {
+            Debug.Log("Scene Load Start");
+        }
+
+        private void TestEnd()
+        {
+            Debug.Log("Scene Load End");
+        }
+
+        private void TestProgress(float progress)
+        {
+            Debug.Log($"Scene Load Progress: {progress:F2}");
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                RequestLoadSingleSceneWithAssetPath(StaticDatas.SCENE_NAME_TITLE);
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                RequestLoadSingleSceneWithAssetPath(StaticDatas.SCENE_NAME_MAIN);
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                RequestLoadSingleSceneWithAssetPath(StaticDatas.SCENE_NAME_GAME);
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                RequestLoadAdditiveSceneWithAssetPath(StaticDatas.SCENE_NAME_ADDITIVE01);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                RequestLoadAdditiveSceneWithAssetPath(StaticDatas.SCENE_NAME_ADDITIVE02);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                RequestLoadAdditiveSceneWithAssetPath(StaticDatas.SCENE_NAME_ADDITIVE03);
+            }
+            else if (Input.GetKeyDown(KeyCode.Z))
+            {
+                RequestUnloadAdditiveSceneWithAssetPath(StaticDatas.SCENE_NAME_ADDITIVE01);
+            }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                RequestUnloadAdditiveSceneWithAssetPath(StaticDatas.SCENE_NAME_ADDITIVE02);
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                RequestUnloadAdditiveSceneWithAssetPath(StaticDatas.SCENE_NAME_ADDITIVE03);
+            }
+        }
+
+        private void RequestLoadSingleSceneWithAssetPath(string sceneName)
+        {
+            string sceneAssetPath = GetScenePath(sceneName);
+
+            if (string.IsNullOrEmpty(sceneAssetPath))
+            {
+                Debug.LogError($"Scene file not found. sceneName: {sceneName}, folder: {StaticDatas.SAMPLE_FOLDER_NAME}");
+                return;
+            }
+
+            SceneLoader.Instance.LoadSingleSceneWithAssetPath(sceneAssetPath);
+        }
+
+        private void RequestLoadAdditiveSceneWithAssetPath(string sceneName)
+        {
+            string sceneAssetPath = GetScenePath(sceneName);
+
+            if (string.IsNullOrEmpty(sceneAssetPath))
+            {
+                Debug.LogError($"Scene file not found. sceneName: {sceneName}, folder: {StaticDatas.SAMPLE_FOLDER_NAME}");
+                return;
+            }
+
+            SceneLoader.Instance.LoadAdditiveSceneWithAssetPath(sceneAssetPath);
+        }
+
+        private void RequestUnloadAdditiveSceneWithAssetPath(string sceneName)
+        {
+            string sceneAssetPath = GetScenePath(sceneName);
+
+            if (string.IsNullOrEmpty(sceneAssetPath))
+            {
+                Debug.LogError($"Scene file not found. sceneName: {sceneName}, folder: {StaticDatas.SAMPLE_FOLDER_NAME}");
+                return;
+            }
+
+            SceneLoader.Instance.UnloadAdditiveSceneWithAssetPath(sceneAssetPath);
+        }
+
+        private string GetScenePath(string sceneName)
+        {
+            string sceneAssetPath = "";
+
+            List<string> folders = FileFinder.FindAllFolders(name: StaticDatas.SAMPLE_FOLDER_NAME);
+            foreach (string folder in folders)
+            {
+                var sceneAssets = FileFinder.LoadAllAssetsAtPath<SceneAsset>(folder);
+                var sceneAsset = sceneAssets.Where(t => t.name == sceneName).FirstOrDefault();
+                if (sceneAsset == null)
+                {
+                    continue;
+                }
+
+                sceneAssetPath = FileFinder.GetAssetPath(sceneAsset);
+                if (!string.IsNullOrEmpty(sceneAssetPath))
+                {
+                    break;
+                }
+            }
+
+            return sceneAssetPath;
+        }
+    }
+}
---
+++ Assets/Mu3LibraryAssets/Samples~/Sample_Game/Scripts/StaticDatas.cs
@@ -0,0 +1,14 @@
+namespace Mu3Library.Sample.Game
+{
+    public static class StaticDatas
+    {
+        public const string SAMPLE_FOLDER_NAME = "Sample_Game";
+
+        public const string SCENE_NAME_TITLE = "Title";
+        public const string SCENE_NAME_MAIN = "Main";
+        public const string SCENE_NAME_GAME = "Game";
+        public const string SCENE_NAME_ADDITIVE01 = "Additive01";
+        public const string SCENE_NAME_ADDITIVE02 = "Additive02";
+        public const string SCENE_NAME_ADDITIVE03 = "Additive03";
+    }
+}
