f81740eb118a6871f31f5fdea323723f92883311
mu3 <74529749+doqltl179@users.noreply.github.com>
2025-10-29 15:24:30 +0900

    Destroy 로그 제거

--- Assets/Mu3LibraryAssets/Scripts/Utility/GenericSingleton.cs
+++ Assets/Mu3LibraryAssets/Scripts/Utility/GenericSingleton.cs
@@ -1,24 +1,34 @@
 using UnityEngine;
 
-namespace Mu3Library.Utility {
-    public class GenericSingleton<T> : MonoBehaviour where T : MonoBehaviour {
-        public static T Instance {
-            get {
-                if(instance == null) {
-                    lock(lockObj) {
+namespace Mu3Library.Utility
+{
+    public class GenericSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObj)
+                    {
                         T[] instances = FindObjectsByType<T>(FindObjectsSortMode.None);
-                        if(instances.Length == 0) {
+                        if (instances.Length == 0)
+                        {
                             GameObject go = new GameObject(typeof(T).Name);
                             instance = go.AddComponent<T>();
                         }
-                        else if(instances.Length == 1) {
+                        else if (instances.Length == 1)
+                        {
                             instance = instances[0];
                         }
-                        else if(instances.Length > 1) {
+                        else if (instances.Length > 1)
+                        {
                             Debug.LogWarning($"'{typeof(T).Name}' already exist more than one.");
 
                             // instance가 두 개 이상이라면 하나만 제외하고 전부 삭제한다.
-                            for(int i = 1; i < instances.Length; i++) {
+                            for (int i = 1; i < instances.Length; i++)
+                            {
                                 Destroy(instances[i].gameObject);
                             }
                             instance = instances[0];
@@ -37,9 +47,8 @@ namespace Mu3Library.Utility {
 
 
 
-        protected virtual void OnDestroy() {
-            Debug.LogWarning($"GenericSingleton Destroyed. type: {typeof(T).Name}");
-
+        protected virtual void OnDestroy()
+        {
             instance = null;
         }
     }
