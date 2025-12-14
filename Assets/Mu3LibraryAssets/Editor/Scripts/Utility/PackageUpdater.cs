2210e83838017049475a81e1a0a15d6939726a5d
mu3 <74529749+doqltl179@users.noreply.github.com>
2025-12-14 23:57:28 +0900

    패키지 업데이트 코드 수정

--- Assets/Mu3LibraryAssets/Editor/Scripts/Utility/PackageUpdater.cs
+++ Assets/Mu3LibraryAssets/Editor/Scripts/Utility/PackageUpdater.cs
@@ -2,13 +2,18 @@ using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using System.Linq;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace Mu3Library.Editor.Utility
 {
     public class PackageUpdater : MonoBehaviour
     {
-        private const string _packageIdentifier = "com.github.doqltl179.mu3libraryassets";
+        private const string _packageNameRoot = "com.github.doqltl179.mu3libraryassets";
+
+        private static ListRequest _listRequest;
         private static AddRequest _updateRequest;
+        private static PackageInfo _lastUpdatedPackageInfo;
 
         private static bool _isUpdating = false;
 
@@ -17,27 +22,59 @@ namespace Mu3Library.Editor.Utility
         [MenuItem("Mu3Library/Update Package")]
         private static void UpdateMyPackage()
         {
-            if(_isUpdating)
+            if (_isUpdating)
             {
                 Debug.LogWarning($"Package updating now...");
                 return;
             }
 
-            _updateRequest = Client.Add(_packageIdentifier);
-            EditorApplication.update += Progress;
+            _listRequest = Client.List(true, false);
+            EditorApplication.update += ListProgress;
         }
 
-        private static void Progress()
+        private static void ListProgress()
         {
-            if (_isUpdating || _updateRequest == null || !_updateRequest.IsCompleted)
+            if (_listRequest == null || !_listRequest.IsCompleted)
+            {
+                return;
+            }
+
+            if (_listRequest.Status == StatusCode.Success)
+            {
+                var result = _listRequest.Result;
+                string packageName = result
+                    .Select(t => t.packageId)
+                    .Where(t => t.Contains(_packageNameRoot))
+                    .FirstOrDefault();
+                if (!string.IsNullOrEmpty(packageName))
+                {
+                    _updateRequest = Client.Add(packageName);
+                    EditorApplication.update += UpdateProgress;
+                }
+            }
+            else
+            {
+                Debug.LogError("Package list not found");
+                _isUpdating = false;
+            }
+
+            _listRequest = null;
+
+            EditorApplication.update -= ListProgress;
+        }
+
+        private static void UpdateProgress()
+        {
+            if (_updateRequest == null || !_updateRequest.IsCompleted)
             {
                 return;
             }
 
             if (_updateRequest.Status == StatusCode.Success)
             {
-                var result = _updateRequest.Result;
-                Debug.Log($"Package update complated. {result.packageId} ({result.version})");
+                _lastUpdatedPackageInfo = _updateRequest.Result;
+
+                EditorApplication.update += PackageUpdateCompleteLog;
             }
             else
             {
@@ -45,8 +82,21 @@ namespace Mu3Library.Editor.Utility
             }
 
             _updateRequest = null;
+            _isUpdating = false;
 
-            EditorApplication.update -= Progress;
+            EditorApplication.update -= UpdateProgress;
+        }
+
+        private static void PackageUpdateCompleteLog()
+        {
+            if (_lastUpdatedPackageInfo == null)
+            {
+                return;
+            }
+
+            Debug.Log($"Package update complated. {_lastUpdatedPackageInfo.packageId} ({_lastUpdatedPackageInfo.version})");
+            
+            EditorApplication.update -= PackageUpdateCompleteLog;
         }
     }
 }
