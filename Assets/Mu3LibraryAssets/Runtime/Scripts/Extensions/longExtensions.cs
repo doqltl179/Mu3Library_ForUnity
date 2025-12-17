8dea33cc16b2cfcb1430780970db82f3dfd6391e
mu3 <74529749+doqltl179@users.noreply.github.com>
2025-12-17 22:05:22 +0900

    longExtensions 추가

---
+++ Assets/Mu3LibraryAssets/Runtime/Scripts/Extensions/longExtensions.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Mu3Library.Extensions
+{
+    public static class longExtensions
+    {
+        public static double BytesToKB(this long value)
+        {
+            return value / 1024f;
+        }
+
+        public static double BytesToMB(this long value)
+        {
+            return value / (1024f * 1024f);
+        }
+
+        public static double BytesToGB(this long value)
+        {
+            return value / (1024f * 1024f * 1024f);
+        }
+    }
+}
