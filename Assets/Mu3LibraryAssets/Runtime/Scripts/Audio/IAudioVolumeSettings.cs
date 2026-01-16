5ba43973c2e9ff2b15b251f37947c49be11c7078
mu3 <74529749+doqltl179@users.noreply.github.com>
2026-01-16 16:15:10 +0900

    volume change event를 IVolumeSettings로 이전

--- Assets/Mu3LibraryAssets/Runtime/Scripts/Audio/IAudioVolumeSettings.cs
+++ Assets/Mu3LibraryAssets/Runtime/Scripts/Audio/IAudioVolumeSettings.cs
@@ -1,12 +1,8 @@
-using System;
 using Mu3Library.Utility.Settings;
 
 namespace Mu3Library.Audio
 {
     public interface IAudioVolumeSettings : IVolumeSettings
     {
-        public event Action<float> OnMasterVolumeChanged;
-        public event Action<float> OnBgmVolumeChanged;
-        public event Action<float> OnSfxVolumeChanged;
     }
 }
--- Assets/Mu3LibraryAssets/Runtime/Scripts/Utility/Settings/IVolumeSettings.cs
+++ Assets/Mu3LibraryAssets/Runtime/Scripts/Utility/Settings/IVolumeSettings.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Mu3Library.Utility.Settings
 {
     public interface IVolumeSettings
@@ -7,6 +9,10 @@ namespace Mu3Library.Utility.Settings
         public float CalculatedBgmVolume { get; }
         public float SfxVolume { get; set; }
         public float CalculatedSfxVolume { get; }
+
+        public event Action<float> OnMasterVolumeChanged;
+        public event Action<float> OnBgmVolumeChanged;
+        public event Action<float> OnSfxVolumeChanged;
 
         public void ResetVolumeAll();
         public void ResetMasterVolume();
