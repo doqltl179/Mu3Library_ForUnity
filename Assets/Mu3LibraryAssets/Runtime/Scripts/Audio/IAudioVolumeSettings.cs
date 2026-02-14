1172f15ec0bc80792ec205e302410433ad574744
mu3 <74529749+doqltl179@users.noreply.github.com>
2026-02-14 21:22:49 +0900

    fix(audio): decouple IAudioVolumeSettings from event bus

--- Assets/Mu3LibraryAssets/Runtime/Scripts/Audio/IAudioVolumeSettings.cs
+++ Assets/Mu3LibraryAssets/Runtime/Scripts/Audio/IAudioVolumeSettings.cs
@@ -2,7 +2,7 @@ using Mu3Library.Utility.Settings;
 
 namespace Mu3Library.Audio
 {
-    public interface IAudioVolumeSettings : IVolumeSettings, IAudioManagerEventBus
+    public interface IAudioVolumeSettings : IVolumeSettings
     {
     }
 }
